26fdd0d0554d5fc8adfae172d3d20cad0f342920
SakakiKomachi <dewadevilzdewa@gmail.com>
2023-06-16 20:15:16 -0700

    perpus commit

--- app/Views/dashboard/header.php
+++ app/Views/dashboard/header.php
@@ -96,7 +96,7 @@
                 </i>
             </a>
         </div>
-        <h1>header</h1>
+        <h1>ini perpus</h1>
     </header>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
